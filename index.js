00e09a0acf0bb45dcf3dc88e0105c827086b049a
Yone5550 <81503945+Yone5550@users.noreply.github.com>
2023-12-11 01:11:03 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,49 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const swagger_ui_express_1 = __importDefault(require("swagger-ui-express"));
+const indexRoutes_1 = __importDefault(require("./routes/indexRoutes"));
+const usuariosRoutes_1 = __importDefault(require("./routes/usuariosRoutes"));
+const empresasRoutes_1 = __importDefault(require("./routes/empresasRoutes"));
+const morgan_1 = __importDefault(require("morgan"));
+const cors_1 = __importDefault(require("cors"));
+const swagger_json_1 = __importDefault(require("./swagger.json"));
+const platicasRoutes_1 = __importDefault(require("./routes/platicasRoutes"));
+const inventariosRoutes_1 = __importDefault(require("./routes/inventariosRoutes"));
+const ventasRoutes_1 = __importDefault(require("./routes/ventasRoutes"));
+const redsRoutes_1 = __importDefault(require("./routes/redsRoutes"));
+class Server {
+    constructor() {
+        this.app = (0, express_1.default)();
+        this.config();
+        this.routes();
+        this.app.use('/documentacion', swagger_ui_express_1.default.serve, swagger_ui_express_1.default.setup(swagger_json_1.default));
+    }
+    config() {
+        this.app.set('port', process.env.PORT || 3306); //En que puerto va a ejecutar
+        this.app.use((0, morgan_1.default)('dev')); //que ejecutamos y que tiempo
+        this.app.use((0, cors_1.default)());
+        this.app.use(express_1.default.json()); //permite que utilicemos json
+        this.app.use(express_1.default.urlencoded({ extended: false })); //decodifca las url
+    }
+    routes() {
+        this.app.use(indexRoutes_1.default);
+        this.app.use('/api/usuarios', usuariosRoutes_1.default);
+        this.app.use('/api/empresas', empresasRoutes_1.default);
+        this.app.use('/api/platicas', platicasRoutes_1.default);
+        this.app.use('/api/inventarios', inventariosRoutes_1.default);
+        this.app.use('/api/ventas', ventasRoutes_1.default);
+        this.app.use('/api/reds', redsRoutes_1.default);
+    }
+    start() {
+        this.app.listen(this.app.get('port'), () => {
+            console.log('El servidor se esta ejecutando en el puerto: ', this.app.get('port'));
+        });
+    }
+}
+const server = new Server();
+server.start();
+//buscan un hostins que soporte nodejs gratis /costo
---
+++ keys.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = {
+    database: {
+        host: 'ls-319753aae48460c8251cc227fb1eeb3507eb0799.c0erz1pgxjmw.us-east-2.rds.amazonaws.com',
+        port: 3306,
+        user: 'dbmasteruser',
+        password: '9|.zo~tFDA<(bp~HZGb{nor70!&Fjng_',
+        database: 'omnilife2.0'
+    }
+};
